e285861aa5d382ae510600d59e8ec0a5d59baadf
Agustino Saling <agustino@saling.eu>
2020-08-03 22:25:25 +0200

    add sample emmet files

---
+++ videos_controlls.js
@@ -0,0 +1,9 @@
+<div className="video">
+  <ul className="video__list">
+    <li className="video__item"><a href=""></a></li>
+    <li className="video__item"><a href=""></a></li>
+    <li className="video__item"><a href=""></a></li>
+    <li className="video__item"><a href=""></a></li>
+    <li className="video__item"><a href=""></a></li>
+  </ul>
+</div>
